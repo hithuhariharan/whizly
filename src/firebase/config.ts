fa1d539eaf9a5caf372561c93286e600a26ecc76
Hithu Hariharan <hithuoff@gmail.com>
2025-10-28 05:32:40 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2597056983-c0fe5",
+  "appId": "1:267270387101:web:7ca497c364f87e654f2b74",
+  "apiKey": "AIzaSyCH3QtMCD8AgQCTtqR9sln83BzXzZUpS90",
+  "authDomain": "studio-2597056983-c0fe5.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "267270387101"
+};
